3bb85338e379ef8747eac6e6dfe4443f2de61136
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-02-15 13:31:50 -0500

    Create RerollAltarDisplay.kt
    (cherry picked from commit 8fe560f7bc63e6fc670f0f479f08499a8bae612d)

---
+++ src/main/kotlin/me/fzzyhmstrs/gearifiers/compat/rei/RerollAltarDisplay.kt
@@ -0,0 +1,48 @@
+package me.fzzyhmstrs.gearifiers.compat.rei
+
+
+import me.shedaniel.rei.api.common.category.CategoryIdentifier
+import me.shedaniel.rei.api.common.display.basic.BasicDisplay
+import me.shedaniel.rei.api.common.entry.EntryIngredient
+import me.shedaniel.rei.api.common.util.EntryStacks
+import net.minecraft.nbt.NbtCompound
+import net.minecraft.util.Identifier
+import java.util.*
+
+class ImbuingTableDisplay(inputs: MutableList<EntryIngredient>, outputs: MutableList<EntryIngredient>, location: Optional<Identifier>):
+    BasicDisplay(inputs, outputs, location) {
+
+    constructor(input: Item, cost: Item): this(
+        getRecipeInputEntries(input,cost)
+        getRecipeOutputEntries(input)
+        getRecipeId(input,cost))
+        
+    override fun getInputEntries(): MutableList<EntryIngredient> {
+        return inputs
+    }
+
+    override fun getOutputEntries(): MutableList<EntryIngredient> {
+        return outputs
+    }
+
+    override fun getCategoryIdentifier(): CategoryIdentifier<*> {
+        return CategoryIdentifier.of(Identifier(Gearifiers.MOD_ID, "rerolling"))
+    }
+
+    companion object{
+
+        private fun getRecipeInputEntries(input: Item, cost: Item): MutableList<EntryIngredient>{
+            return mutableListOf(EntryIngredients.of(input),EntryIngredients.of(cost))
+        }
+
+        private fun getRecipeOutputEntries(input: Item): MutableList<EntryIngredient>{
+            return mutableListOf(EntryIngredients.of(input))
+        }
+        
+        private fun getRecipeId(input: Item, cost: Item): Optional<Identifier>{
+            val itemId1 = Registries.ITEM.getId(input)
+            val itemId2 = Registries.ITEM.getId(cost)
+            return Optional.ofNullable(Identifier(Gearifiers.MOD_ID,itemId1.namespace + "." + itemId1.path + "/paid_with/" + itemId2.namespace + "." + itemId2.path))  
+        }
+    }
+}
