9a0f403782020f248aa50faf5fb7f851164aedbd
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-06-03 20:08:21 -0400

    fixes for build

--- src/main/kotlin/me/fzzyhmstrs/gearifiers/registry/RegisterItem.kt
+++ src/main/kotlin/me/fzzyhmstrs/gearifiers/registry/RegisterItem.kt
@@ -4,27 +4,27 @@ import me.fzzyhmstrs.gearifiers.Gearifiers
 import me.fzzyhmstrs.gearifiers.item.*
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.minecraft.item.Item
-import net.minecraft.registry.Registries
-import net.minecraft.registry.Registry
+import net.minecraft.item.ItemGroup
 import net.minecraft.util.Identifier
 import net.minecraft.util.Rarity
+import net.minecraft.util.registry.Registry
 
 object RegisterItem {
 
     internal val regItem:MutableMap<String, Item> = mutableMapOf()
 
-    val REPAIR_KIT = RepairKitItem(FabricItemSettings().maxCount(1)).also { regItem["repair_kit"] = it }
-    val SEAL_OF_AWAKENING = SealOfAwakeningItem(FabricItemSettings().maxCount(1).rarity(Rarity.UNCOMMON)).also { regItem["seal_of_awakening"] = it }
-    val SEAL_OF_CHAOS = SealOfChaosItem(FabricItemSettings().maxCount(1).rarity(Rarity.UNCOMMON)).also { regItem["seal_of_chaos"] = it }
-    val SEAL_OF_CLEANSING = SealOfCleansingItem(FabricItemSettings().maxCount(1).rarity(Rarity.UNCOMMON)).also { regItem["seal_of_cleansing"] = it }
-    val SEAL_OF_LEGENDS = SealOfLegendsItem(FabricItemSettings().maxCount(1).rarity(Rarity.EPIC)).withGlint().also { regItem["seal_of_legends"] = it }
-    val SEAL_OF_TRANSFERAL = SealOfTransferalItem(FabricItemSettings().maxCount(1).rarity(Rarity.EPIC)).withGlint().also { regItem["seal_of_transferal"] = it }
-    val SEAL_OF_WIPING = SealOfWipingItem(FabricItemSettings().maxCount(1).rarity(Rarity.EPIC)).withGlint().also { regItem["seal_of_wiping"] = it }
-    val WHETSTONE = WhetstoneItem(FabricItemSettings().maxCount(1)).also { regItem["whetstone"] = it }
+    val REPAIR_KIT = RepairKitItem(FabricItemSettings().maxCount(1).group(ItemGroup.TOOLS)).also { regItem["repair_kit"] = it }
+    val SEAL_OF_AWAKENING = SealOfAwakeningItem(FabricItemSettings().maxCount(1).rarity(Rarity.UNCOMMON).group(ItemGroup.TOOLS)).also { regItem["seal_of_awakening"] = it }
+    val SEAL_OF_CHAOS = SealOfChaosItem(FabricItemSettings().maxCount(1).rarity(Rarity.UNCOMMON).group(ItemGroup.TOOLS)).also { regItem["seal_of_chaos"] = it }
+    val SEAL_OF_CLEANSING = SealOfCleansingItem(FabricItemSettings().maxCount(1).rarity(Rarity.UNCOMMON).group(ItemGroup.TOOLS)).also { regItem["seal_of_cleansing"] = it }
+    val SEAL_OF_LEGENDS = SealOfLegendsItem(FabricItemSettings().maxCount(1).rarity(Rarity.EPIC).group(ItemGroup.TOOLS)).withGlint().also { regItem["seal_of_legends"] = it }
+    val SEAL_OF_TRANSFERAL = SealOfTransferalItem(FabricItemSettings().maxCount(1).rarity(Rarity.EPIC).group(ItemGroup.TOOLS)).withGlint().also { regItem["seal_of_transferal"] = it }
+    val SEAL_OF_WIPING = SealOfWipingItem(FabricItemSettings().maxCount(1).rarity(Rarity.EPIC).group(ItemGroup.TOOLS)).withGlint().also { regItem["seal_of_wiping"] = it }
+    val WHETSTONE = WhetstoneItem(FabricItemSettings().maxCount(1).group(ItemGroup.TOOLS)).also { regItem["whetstone"] = it }
 
     fun registerAll(){
         for (entry in regItem){
-            Registry.register(Registries.ITEM, Identifier(Gearifiers.MOD_ID,entry.key), entry.value)
+            Registry.register(Registry.ITEM, Identifier(Gearifiers.MOD_ID,entry.key), entry.value)
         }
     }
 
