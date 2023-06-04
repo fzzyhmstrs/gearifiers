8edf35ace9273244d177992c44cf4c973ab7836d
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-06-03 20:53:21 -0400

    1.19 add Seal of Fate to reg

--- src/main/kotlin/me/fzzyhmstrs/gearifiers/registry/RegisterItem.kt
+++ src/main/kotlin/me/fzzyhmstrs/gearifiers/registry/RegisterItem.kt
@@ -15,8 +15,9 @@ object RegisterItem {
 
     val REPAIR_KIT = RepairKitItem(FabricItemSettings().maxCount(1).group(ItemGroup.TOOLS)).also { regItem["repair_kit"] = it }
     val SEAL_OF_AWAKENING = SealOfAwakeningItem(FabricItemSettings().maxCount(1).rarity(Rarity.UNCOMMON).group(ItemGroup.TOOLS)).also { regItem["seal_of_awakening"] = it }
-    val SEAL_OF_CHAOS = SealOfChaosItem(FabricItemSettings().maxCount(1).rarity(Rarity.UNCOMMON).group(ItemGroup.TOOLS)).also { regItem["seal_of_chaos"] = it }
+    val SEAL_OF_CHAOS = SealOfChaosItem(FabricItemSettings().maxCount(1).rarity(Rarity.RARE).group(ItemGroup.TOOLS)).also { regItem["seal_of_chaos"] = it }
     val SEAL_OF_CLEANSING = SealOfCleansingItem(FabricItemSettings().maxCount(1).rarity(Rarity.UNCOMMON).group(ItemGroup.TOOLS)).also { regItem["seal_of_cleansing"] = it }
+    val SEAL_OF_FATE = SealOfFateItem(FabricItemSettings().maxCount(1).rarity(Rarity.UNCOMMON).group(ItemGroup.TOOLS)).also { regItem["seal_of_fate"] = it }
     val SEAL_OF_LEGENDS = SealOfLegendsItem(FabricItemSettings().maxCount(1).rarity(Rarity.EPIC).group(ItemGroup.TOOLS)).withGlint().also { regItem["seal_of_legends"] = it }
     val SEAL_OF_TRANSFERAL = SealOfTransferalItem(FabricItemSettings().maxCount(1).rarity(Rarity.EPIC).group(ItemGroup.TOOLS)).withGlint().also { regItem["seal_of_transferal"] = it }
     val SEAL_OF_WIPING = SealOfWipingItem(FabricItemSettings().maxCount(1).rarity(Rarity.EPIC).group(ItemGroup.TOOLS)).withGlint().also { regItem["seal_of_wiping"] = it }
