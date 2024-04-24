3610d74a2d437643d3018df24b0f8552dc409267
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-04-24 11:59:57 -0400

    Update SealOfChaosItem.kt

--- src/main/kotlin/me/fzzyhmstrs/gearifiers/item/SealOfChaosItem.kt
+++ src/main/kotlin/me/fzzyhmstrs/gearifiers/item/SealOfChaosItem.kt
@@ -21,7 +21,7 @@ class SealOfChaosItem(settings: Settings): ModifierAffectingItem(settings) {
         hand: Hand
     ): TypedActionResult<ItemStack> {
         if (world.isClient) return TypedActionResult.pass(modifierAffectingItem)
-        if (GearifiersConfig.blackList.isItemBlackListed(stack)) {
+        if (GearifiersConfigNew.getInstance().isItemBlackListed(stack)) {
             user.sendMessage(AcText.translatable("item.gearifiers.seals.blacklisted"))
             return TypedActionResult.pass(modifierAffectingItem)
         }
